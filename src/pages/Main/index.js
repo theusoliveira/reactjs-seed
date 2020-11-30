cc7944c8b9edf166ad9a16e22450c0162dfc80f3
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2020-11-30 15:25:00 -0300

    Update seed

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <h1>Welcome to ReactJS Seed!</h1>
+    </Container>
+  );
+}
+
+export default Main;
